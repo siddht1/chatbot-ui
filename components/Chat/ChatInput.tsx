0d6fb539b81d8a4cb82c510e1a013e585d4fc66b
siddht1 <rt1yrru+github@gmail.com>
2023-06-21 19:13:55 +0530

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -311,7 +311,7 @@ export const ChatInput: FC<Props> = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/siddht1/chatbot-u"
+          href="https://github.com/siddht1/chatbot-ui"
           target="_blank"
           rel="noreferrer"
           className="underline"
